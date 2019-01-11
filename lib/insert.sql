a81750d1c7e3c0b75822e60a0c9b571a123a43df
shannon-nabors <shannon.nabors@gmail.com>
2019-01-11 16:16:51 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'white', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 8, 'female', 'brown', 'placid', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'female', 'brown', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'female', 'black', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'female', 'white', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 8, 'male', 'brown', 'rowdy', 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'female', 'black', 'rowdy', 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 4, 'male', 'white', 'rowdy', 0);
